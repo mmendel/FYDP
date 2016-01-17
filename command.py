8186f37f09a1d07f95c8cdcfefc7cfa4bc6f09f1
Mathew <mathew.mend@gmail.com>
2016-01-16 23:21:06 -0500

    Added new commands to control streaming

--- command.py
+++ command.py
@@ -9,33 +9,34 @@ def my_print(text):
 	sys.stdout.write(str(text))
 	sys.stdout.flush()
 
-#Note: 0 index used as not applicable
 class Joint:
-	PIP = 1
-	MCPV = 2
-	MCPH = 3
+	PIP = 0
+	MCPV = 1
+	MCPH = 2
 
 class Finger:
-	INDEX = 1
-	MIDDLE = 2
-	RING = 3
-	PINKY = 4
+	INDEX = 0
+	MIDDLE = 1
+	RING = 2
+	PINKY = 3
 
 class Commands:
-	START = 1
-	STOP = 2
+	STOP = 1
+	START = 2
 	SET = 3
 	GET = 4
+	STREAMSTART = 5
+	STREAMSTOP = 6
 
 
 class SCICommandInterface:
 	ser = None
 
-	def __init__ (self, port='com6', baudrate=9600, serIn = None):
+	def __init__ (self, port='com6', baudrate=9600, serIn = None, timeoutIn = 5):
 		if serIn is not None:
 			self.ser = serIn
 		else:
-			self.ser = serial.Serial(port, baudrate)
+			self.ser = serial.Serial(port, baudrate, timeout = timeoutIn)
 	
 	def SendCommand(self, command, finger = 0, joint = 0, value = 0):
 		msg = struct.pack('BBB', command, (finger << 4) + joint, value)
@@ -46,11 +47,19 @@ class SCICommandInterface:
 		self.SendCommand(Commands.STOP)	
 	
 	def AllStart(self):
-		self.SendCommand(Dommands.START)
+		self.SendCommand(Commands.START)
 
 	def SetFingerJoint(self, nFinger, nJoint, dVal):
 		self.SendCommand(Commands.SET, nFinger, nJoint, dVal)
 
 	def GetFingerJoint(self, nFinger, nJoint):
 		self.SendCommand(Commands.GET, nFinger, nJoint)
+		print struct.unpack('B', self.ser.read())
+	
+	def StreamStart(self, nFinger, nJoint):
+		self.SendCommand(Commands.STREAMSTART, nFinger, nJoint)
+	
+	def StreamStop(self, nFinger, nJoint):
+		self.SendCommand(Commands.STREAMSTOP, nFinger, nJoint)
+
 
