e42bfa1f5541eb61f5f68aba9361da171448e195
Mathew <mathew.mend@gmail.com>
2016-02-24 22:58:18 -0500

    Stream now takes command line args

--- stream.py
+++ stream.py
@@ -4,6 +4,7 @@ import struct
 import time
 import command
 import datetime
+import argparse
 
 def my_print(text):
 	sys.stdout.write(str(text))
@@ -11,35 +12,39 @@ def my_print(text):
 
 
 
-_port = 'com6'
-pos = 70 
-finger = 0
-joint = 0
+def stream(finger, joint, pos, portin):
+	filename = 'Finger-' + str(finger) + ' Joint-' + str(joint) + ' Pos-' + str(pos) + ' ' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.txt'
+	my_print(filename)
+	with open(filename, "a") as f:
+		f.write(filename + '\n')
+	c = command.SCICommandInterface(port = portin)
 
-filename = 'Finger-' + str(finger) + ' Joint-' + str(joint) + ' Pos-' + str(pos) + ' ' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.txt'
-my_print(filename)
-with open(filename, "a") as f:
-	f.write(filename + '\n')
-c = command.SCICommandInterface(port = _port)
+	c.SetFingerJoint(finger, joint, pos)
+	c.StreamStart(finger, joint)
 
-c.SetFingerJoint(finger, joint, pos)
-c.StreamStart(finger, joint)
-
-c.AllStart()
-c.ser.flush()
-while 1:
-	line = c.ser.readline()
-	if line[0] == ":":
-		line = line.replace(":", "")
-		line = line.replace("\r\n", "")
-		try:
-			val = struct.unpack('>f', line)[0]
-			my_print(str(val) + '\n')
-			with open(filename, "a") as f:
-				f.write(str(val) + '\n')			
-		except:
-			pass
-				
+	c.AllStart()
+	c.ser.flush()
+	while 1:
+		line = c.ser.readline()
+		if line[0] == ":":
+			line = line.replace(":", "")
+			line = line.replace("\r\n", "")
+			try:
+				val = struct.unpack('>f', line)[0]
+				my_print(str(val) + '\n')
+				with open(filename, "a") as f:
+					f.write(str(val) + '\n')			
+			except:
+				pass
+					
 
 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('Finger', metavar='F', type=int)
+	parser.add_argument('Joint', metavar='J', type=int)
+	parser.add_argument('Posistion', metavar='P', type=int)
+	parser.add_argument('--port', default = 'com6')
+	args = parser.parse_args()
+	stream(args.Finger, args.Joint, args.Posistion, args.port)
 
