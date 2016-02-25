f88f93c3be22b716bad2998cb9a1d920b478854c
Mathew <mathew.mend@gmail.com>
2016-02-24 21:48:04 -0500

    LED Control

---
+++ FYDP_C2000/LED.c
@@ -0,0 +1,48 @@
+/*
+ * LED.c
+ *
+ *  Created on: Feb 24, 2016
+ *      Author: Mathew
+ */
+
+#include "LED.h"
+
+
+void ConfigureLeds(GPIO_Handle myGpioIn)
+{
+	myGpioCopy = myGpioIn;
+
+	// Set to general purpose
+    GPIO_setMode(myGpioCopy, POWER, 0);
+    GPIO_setMode(myGpioCopy, RUNNING, 0);
+    GPIO_setMode(myGpioCopy, ERROR, 0);
+    GPIO_setMode(myGpioCopy, KILLSWITCH, 0);
+    GPIO_setMode(myGpioCopy, STREAM, 0);
+    GPIO_setMode(myGpioCopy, LED5, 0);
+
+    // Set all as outpus
+    GPIO_setDirection(myGpioCopy, POWER, GPIO_Direction_Output);
+    GPIO_setDirection(myGpioCopy, RUNNING, GPIO_Direction_Output);
+    GPIO_setDirection(myGpioCopy, ERROR, GPIO_Direction_Output);
+    GPIO_setDirection(myGpioCopy, KILLSWITCH, GPIO_Direction_Output);
+    GPIO_setDirection(myGpioCopy, STREAM, GPIO_Direction_Output);
+    GPIO_setDirection(myGpioCopy, LED5, GPIO_Direction_Output);
+
+    // Disable all
+    GPIO_setLow(myGpioCopy, POWER);
+    GPIO_setLow(myGpioCopy, RUNNING);
+    GPIO_setLow(myGpioCopy, ERROR);
+    GPIO_setLow(myGpioCopy, KILLSWITCH);
+    GPIO_setLow(myGpioCopy, STREAM);
+    GPIO_setLow(myGpioCopy, LED5);
+}
+
+void SetLedState(int led, int state)
+{
+	if (state == ON)
+	    GPIO_setHigh(myGpioCopy, led);
+	else
+	    GPIO_setLow(myGpioCopy, led);
+}
+
+
---
+++ FYDP_C2000/LED.h
@@ -0,0 +1,31 @@
+/*
+ * LED.h
+ *
+ *  Created on: Feb 24, 2016
+ *      Author: Mathew
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+#include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
+#include "f2802x_common/include/gpio.h"
+
+#define POWER	 	GPIO_Number_19
+#define RUNNING 	GPIO_Number_16
+#define ERROR 		GPIO_Number_17
+#define KILLSWITCH 	GPIO_Number_32
+#define STREAM 		GPIO_Number_33
+#define LED5		GPIO_Number_18
+
+#define ON			1
+#define OFF			6
+
+GPIO_Handle myGpioCopy;
+
+void ConfigureLeds(GPIO_Handle myGpio);
+void SetLedState(int led, int state);
+
+
+#endif /* LED_H_ */
+
